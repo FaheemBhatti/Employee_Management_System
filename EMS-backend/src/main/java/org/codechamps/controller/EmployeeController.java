06cf5a2d683d32c9566202121208ca0257ec34e8
FaheemBhatti <mefaheemazfar@gmail.com>
2021-04-02 17:52:45 +0200

    Employee List RESTAPI

--- EMS-backend/src/main/java/org/codechamps/controller/EmployeeController.java
+++ EMS-backend/src/main/java/org/codechamps/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.codechamps.entity.Employee;
 import org.codechamps.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1")
+@CrossOrigin(exposedHeaders="Access-Control-Allow-Origin")
 public class EmployeeController 
 {
 	@Autowired
